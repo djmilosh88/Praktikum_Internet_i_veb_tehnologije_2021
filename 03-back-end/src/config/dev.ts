575e626661250155bf9e83e54fae710597b90c11
Milos Djokic <milos.djokic.15@singimail.rs>
2022-02-06 18:40:17 +0100

    Implement the database connection and use it in the category service

--- 03-back-end/src/config/dev.ts
+++ 03-back-end/src/config/dev.ts
@@ -1,4 +1,4 @@
-import IConfig from './IConfig.interface';
+import IConfig from '../common/IConfig.interface';
 const Config: IConfig = {
     server: {
         port: 40080,
@@ -11,6 +11,16 @@ const Config: IConfig = {
             index: false,
             maxAge: 3600000,
         },
+    },
+
+    database: {
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "root",
+        database: "aplikacija",
+        charset: "utf8",
+        timezone: "+01:00",
     },
 };
 
